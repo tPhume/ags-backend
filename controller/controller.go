126a83181c2d1cb56d36e3aad065276ebec90507
Traphume <traphumedev@gmail.com>
2020-03-17 16:18:43 +0700

    Added Controller type. Added Controller type struct validation. Added function to register validation to Gin's Engine.

--- controller/controller.go
+++ controller/controller.go
@@ -1,1 +1,37 @@
 package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/google/uuid"
+	"gopkg.in/go-playground/validator.v9"
+	"strings"
+)
+
+// Controller represent a Raspberry Pi unit
+type Controller struct {
+	ID   string `json:"id" validate:"required"`
+	Name string `json:"name" validate:"required"`
+	Desc string `json:"desc"`
+	Plan string `json:"plan"`
+}
+
+// Custom Controller type struct validation method
+func StructValidation(sl validator.StructLevel) {
+	controller := sl.Current().Interface().(Controller)
+
+	if _, err := uuid.Parse(controller.ID); err != nil {
+		sl.ReportError(controller.ID, "id", "id", "", "")
+	}
+
+	if len(strings.TrimSpace(controller.Name)) < 1 {
+		sl.ReportError(controller.Name, "name", "name", "", "")
+	}
+}
+
+// Function to add ControllerStructValidation to Gin's default Validator engine
+func addStructValidation(engine *gin.Engine) {
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterStructValidation(StructValidation)
+	}
+}
