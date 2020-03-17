af811b2fb90ca4822fb3b3e6caa855b2cc7f1673
Traphume <traphumedev@gmail.com>
2020-03-17 21:04:36 +0700

    Added Repo interface. Added UserId to entity. Refactor code

--- controller/controller.go
+++ controller/controller.go
@@ -8,30 +8,62 @@ import (
 	"strings"
 )
 
-// Controller represent a Raspberry Pi unit
-type Controller struct {
-	ID   string `json:"id" validate:"required"`
-	Name string `json:"name" validate:"required"`
-	Desc string `json:"desc"`
-	Plan string `json:"plan"`
+// Controller Entity type represent edge device
+type Entity struct {
+	ControllerId string `json:"controller_id" validate:"required"`
+	UserId       string `json:"user_id" validate:"required"`
+	Name         string `json:"name" validate:"required"`
+	Desc         string `json:"desc"`
+	Plan         string `json:"plan"`
 }
 
-// Custom Controller type struct validation method
+// Custom Controller type struct validation function
 func StructValidation(sl validator.StructLevel) {
-	controller := sl.Current().Interface().(Controller)
+	entity := sl.Current().Interface().(Entity)
 
-	if _, err := uuid.Parse(controller.ID); err != nil {
-		sl.ReportError(controller.ID, "id", "id", "", "")
+	// checks ControllerId
+	if _, err := uuid.Parse(entity.ControllerId); err != nil {
+		sl.ReportError(entity.ControllerId, "ControllerId", "ControllerId", "", "")
 	}
 
-	if len(strings.TrimSpace(controller.Name)) < 1 {
-		sl.ReportError(controller.Name, "name", "name", "", "")
+	// checks UserId
+	if _, err := uuid.Parse(entity.UserId); err != nil {
+		sl.ReportError(entity.UserId, "UserId", "UserId", "", "")
+	}
+
+	// checks name
+	if len(strings.TrimSpace(entity.Name)) < 1 {
+		sl.ReportError(entity.Name, "name", "name", "", "")
 	}
 }
 
-// Function to add ControllerStructValidation to Gin's default Validator engine
+// addStructValidation register StructValidation function to Gin's default validator Engine
 func addStructValidation(engine *gin.Engine) {
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
-		v.RegisterStructValidation(StructValidation, Controller{})
+		v.RegisterStructValidation(StructValidation, Entity{})
 	}
+}
+
+// Controller Repo - interface to communicate with data source
+type Repo interface {
+	// AddController creates new controller at data source given *Entity type
+	// Duplicated Controller entity will result in an error
+	AddController(entity *Entity) error
+
+	// ListControllers fetches all controller under the given UserId
+	// Return of empty slice does not imply error
+	ListControllers(string) ([]*Entity, error)
+
+	// GetController fetches specific controller by given ControllerId
+	// Return of nil value for *Entity indicates error
+	GetController(string) (*Entity, error)
+
+	// UpdateController does partial update given *Controller type
+	// No new controller is created if controller is not found
+	UpdateController(*Entity) error
+
+	// RemoveController deletes data from data source given ControllerId
+	// Cascade deletion is done asynchronously
+	// Missing controller will result in an error
+	RemoveController(string) error
 }
--- controller/controller_test.go
+++ controller/controller_test.go
@@ -5,16 +5,17 @@ import (
 	"testing"
 )
 
-var controller = Controller{
-	ID:   "f1d67e51-4ca4-4b25-a4b7-6c8f06822075",
-	Name: "Controller",
-	Desc: "",
-	Plan: "",
+var controller = Entity{
+	ControllerId: "f1d67e51-4ca4-4b25-a4b7-6c8f06822075",
+	UserId:       "76de6d55-e457-4070-8aef-5633726d498f",
+	Name:         "Controller",
+	Desc:         "",
+	Plan:         "",
 }
 
 func TestStructValidation(t *testing.T) {
 	v := validator.New()
-	v.RegisterStructValidation(StructValidation, Controller{})
+	v.RegisterStructValidation(StructValidation, Entity{})
 
 	if err := v.Struct(controller); err != nil {
 		t.Fatal(err)
