7e48e2f5a63709f7cbea5ba4cc0841edd4d53986
Traphume <traphumedev@gmail.com>
2020-03-17 22:56:13 +0700

    Added Handler type and AddController method. Added standard error for Repo interface.

--- controller/controller.go
+++ controller/controller.go
@@ -3,10 +3,12 @@
 package controller
 
 import (
+	"errors"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/google/uuid"
 	"gopkg.in/go-playground/validator.v9"
+	"net/http"
 	"strings"
 )
 
@@ -68,4 +70,66 @@ type Repo interface {
 	// Cascade deletion is done asynchronously
 	// Missing controller will result in an error
 	RemoveController(string) error
+}
+
+// Contains errors that implementation of Repo should use
+var (
+	duplicateName      = errors.New("duplicate name")
+	controllerNotFound = errors.New("controller not found")
+)
+
+// Handler for controller REST API
+type Handler struct {
+	repo Repo
+}
+
+var (
+	keyNotFound = errors.New("key not found")
+	castingFail = errors.New("casting fail")
+)
+
+func (h *Handler) AddController(ctx *gin.Context) {
+	userId, err := getUserId(ctx)
+	if err != nil {
+		ctx.Status(http.StatusInternalServerError)
+		return
+	}
+
+	entity := &Entity{
+		ControllerId: uuid.New().String(),
+		UserId:       userId,
+	}
+
+	if err = ctx.ShouldBindJSON(entity); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": "invalid values"})
+		return
+	}
+
+	if err = h.repo.AddController(entity); err != nil {
+		if err == duplicateName {
+			ctx.JSON(http.StatusBadRequest, gin.H{"message": "duplicate name"})
+		} else {
+			ctx.Status(http.StatusInternalServerError)
+		}
+
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, gin.H{"message": "controller added", "controller_id": entity.ControllerId})
+}
+
+// Helper function that returns userId from context
+func getUserId(ctx *gin.Context) (string, error) {
+	// get userId
+	v, exist := ctx.Get("userId")
+	if !exist {
+		return "", keyNotFound
+	}
+
+	userId, ok := v.(string)
+	if !ok {
+		return "", castingFail
+	}
+
+	return userId, nil
 }
