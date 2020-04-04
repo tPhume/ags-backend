048e3bac72d730a53612ce6483fd7babc98528b1
Traphume <traphumedev@gmail.com>
2020-04-04 17:29:49 +0700

    Refactor to use Redis as session store instead

---
+++ session/redis_mongo.go
@@ -0,0 +1,45 @@
+package session
+
+import (
+	"context"
+	"github.com/go-redis/redis/v7"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"time"
+)
+
+type RedisMongo struct {
+	userDb    *mongo.Collection
+	sessionDb *redis.Client
+}
+
+func (r *RedisMongo) CreateSession(ctx context.Context, userEntity *UserEntity, sessionId string) error {
+	// Upsert user entity
+	opt := options.Replace()
+	opt.SetUpsert(true)
+
+	if _, err := r.userDb.ReplaceOne(ctx, mapping{"_id": userEntity.UserId}, mapping{
+		"email":          userEntity.Email,
+		"email_verified": userEntity.EmailVerified,
+		"name":           userEntity.Name,
+		"picture":        userEntity.Picture,
+	}, opt); err != nil {
+		return err
+	}
+
+	// Create new session
+	if err := r.sessionDb.Set(sessionId, userEntity.UserId, time.Hour*10).Err(); err != nil {
+		return err
+	}
+
+	
+	return nil
+}
+
+func (r *RedisMongo) DeleteSession(ctx context.Context, sessionId string) error {
+	return nil
+}
+
+func (r *RedisMongo) GetUser(ctx context.Context, sessionId string) (string, error) {
+	return "", nil
+}
