d8953d11aa436492b3ac2040be6be59ae515b6cc
Traphume <traphumedev@gmail.com>
2020-04-04 21:39:32 +0700

    Fixed example

--- cmd/session/session.go
+++ cmd/session/session.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis/v7"
 	"github.com/spf13/viper"
@@ -37,6 +38,9 @@ func main() {
 	mongoClient, err := mongo.NewClient(options.Client().ApplyURI(viper.GetString("MONGO_URI")))
 	failOnError("could not create mongodb client", err)
 
+	err = mongoClient.Connect(context.Background())
+	failOnError("could not create connection to mongodb", err)
+
 	db := viper.GetString("MONGO_DATABASE")
 	col := viper.GetString("MONGO_COLLECTION")
 
@@ -59,6 +63,15 @@ func main() {
 	// Init Gin Engine
 	engine := gin.New()
 	session.RegisterRoutes(handler, engine)
+
+	engine.GET("/ping", func(ctx *gin.Context) {
+		if err := mongoClient.Ping(ctx, nil); err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"message": "database not connected"})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, gin.H{"message": "ready"})
+	})
 
 	ping := engine.Group("/api/v1/session/ping")
 	ping.Use(handler.GetUser)
